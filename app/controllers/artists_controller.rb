d570e2c931013aa60f8476ce0dbd578046f2143e
julientregoat <julienctregoat@gmail.com>
2018-01-15 14:36:02 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,27 @@
 class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+    render 'form'
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+    render 'form'
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,26 @@
 class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+    render 'form'
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+    render 'form'
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
 end
