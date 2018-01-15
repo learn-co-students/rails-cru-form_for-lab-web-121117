83e60bfcb84b042c9911689cbb3184e895f0b3fd
krandles <krandles@gmail.com>
2018-01-14 19:55:00 -0500

    Done.

---
+++ db/migrate/20180114225122_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.text :bio
+      t.belongs_to :artist, index: true, foreign_key: true
+      t.belongs_to :genre, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
